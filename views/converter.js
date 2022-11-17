1aa91dfa885eef4a4db953efb2084c38b4b4ca02
BensinBosse <kallehaljerod@gmail.com>
2022-11-17 14:07:07 +0100

    Combined Array with Sql query

--- views/converter.js
+++ views/converter.js
@@ -9,23 +9,29 @@ function mdfinder() {
     const content = fs.readFileSync(folder + explorer[file], "utf8");
     const website = md2json.parse(content);
     pageArr[file] = {
-      name: explorer[file].replace(".md", ""),
-      content: website,
+      pageName: explorer[file].replace(".md", ""),
+      pageContent: website,
     };
   }
 
   //! Lägg varje file i variabel, converta variabeln
 }
 mdfinder();
-console.log(pageArr);
 
 const sqlite3 = require("sqlite3").verbose();
 let db = new sqlite3.Database("../db/themes.db");
 
 db.run(
-  "INSERT INTO molekylverkstan (page_name, page_content) VALUES (" +
-    pageName +
-    ", " +
-    pageContent +
-    ")"
+  "DELETE FROM molekylverkstan",
+  "CREATE TABLE [IF NOT EXISTS] 'molekylverkstan' (page_id INTEGER PRIMARY KEY, page_name TEXT NOT NULL, page_content TEXT NOT NULL);"
 );
+for (i = 0; i < pageArr.length - 1; i++) {
+  console.log(pageArr[i].pageName);
+  db.run(
+    "INSERT INTO molekylverkstan (page_name, page_content) VALUES ('" +
+      pageArr[i].pageName +
+      "', '" +
+      pageArr[i].pageContent +
+      "')"
+  );
+}
