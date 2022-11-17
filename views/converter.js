1cb40554219fbdbf0358777844f0c691e94f2d24
BensinBosse <kallehaljerod@gmail.com>
2022-11-17 13:38:26 +0100

    Markdown files are taken from the markdown folder, the name and contents are stored in objects in an array.

--- views/converter.js
+++ views/converter.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
 const explorer = fs.readdirSync("../pages/markdown");
 const md2json = require("md-2-json");
+const pageArr = [explorer.length];
 
-const folder = "../pages/markdown/";
-for (const file in explorer) {
-  const content = fs.readFileSync(folder + explorer[file], "utf8");
-  console.log(md2json.parse(content));
+function mdfinder() {
+  const folder = "../pages/markdown/";
+  for (const file in explorer) {
+    const content = fs.readFileSync(folder + explorer[file], "utf8");
+    const website = md2json.parse(content);
+    pageArr[file] = {
+      name: explorer[file].replace(".md", ""),
+      content: website,
+    };
+  }
 
   //! Lägg varje file i variabel, converta variabeln
 }
+mdfinder();
+console.log(pageArr);
