ff1c05af507e052717574e5b3ce818746d4a95e9
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-11-21 12:28:02 +0100

    Removed unnecessary dependencies

--- controller/router.js
+++ controller/router.js
@@ -6,4 +6,10 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", session: req.session });
 });
 
+router.get("/:id", function (req, res) {
+  const { id } = req.params;
+  // res.status(200).render(`..pages/air/${id}`);
+  res.send(req.params);
+});
+
 export default router;
