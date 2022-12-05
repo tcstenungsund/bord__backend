c92af6d7a675fbfa6e1b14bce8f28a2fedaebe0c
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 10:44:57 +0100

    fixat credentials...?

--- controller/controller.js
+++ controller/controller.js
@@ -11,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://pontusolsson03.github.io");
   res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
