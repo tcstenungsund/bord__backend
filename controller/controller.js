c75eee55385fac4820873f877fa82aa8679ec79e
pontusolsson03 <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 17:27:33 +0100

    slutade försöka

--- controller/controller.js
+++ controller/controller.js
@@ -10,7 +10,6 @@ app.set("view engine", "ejs");
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://pontusolsson03.github.io");
-  res.header("Access-Control-Allow-Origin", "https://127.0.0.1:8080");
   res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
