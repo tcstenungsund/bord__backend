260b9726e390f33d33cf9f9fc81025b0a3223c96
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 14:39:59 +0100

    rvr

--- views/converter.js
+++ views/converter.js
@@ -4,8 +4,6 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./db/themes.db");
 
 export function converter(explorer, folder, user) {
-  // const explorer = fs.readdirSync("./html/fruit");
-  // const folder = "./html/fruit/";
   const pageArr = [explorer.length];
 
   for (const file in explorer) {
