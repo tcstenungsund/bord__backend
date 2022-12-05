be4d16bd14e286062a97c4ad5726973af181fe87
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 09:04:39 +0100

    Försök igen

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,22 @@
 import express from "express";
-// import sexyRequire from "sexy-require";
 const app = express();
 const PORT = 8080;
 app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
 
 app.use(express.json());
+app.use(cors({ origin: "http://127.0.0.1:8080/", credentials: true }));
 app.use(express.static("start"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //* These are the routers in use, who in turn refer to the .js files in "routes"
 import router from "../controller/router.js";
