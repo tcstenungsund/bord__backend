093e24a7187749480b78c61c8ad91df2950297d2
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 13:59:47 +0100

    converter should now work

--- controller/router.js
+++ controller/router.js
@@ -1,20 +1,26 @@
 import express from "express";
+import fs from "fs";
 import { fetchContent } from "../model/model.js";
-// import { converter } from "../views/converter.js";
+import { converter } from "../views/converter.js";
 
 const router = express.Router();
 
 //* Sends out the ejs (basically HTML) on start URL "localhost:8080"
 router.get("/", function (req, res) {
-  //converter();
   res.render("../pages/start.ejs");
 });
 
 //* Querys the database, the table ":id"
 router.get("/:userId", async function (req, res, next) {
   let userId = req.params.userId;
-  let sqlQuery = `SELECT page_content FROM ${userId} WHERE page_name = "about";`;
-  const content = await fetchContent(sqlQuery);
+  const explorer = fs.readdirSync(`./html/${userId}`);
+  const folder = `./html/${userId}/`;
+  if (userId !== "favicon.ico") {
+    converter(explorer, folder, userId);
+  }
+
+  let fetchQuery = `SELECT page_content FROM ${userId} WHERE page_name = "about";`;
+  const content = await fetchContent(fetchQuery);
   if (content == "404") {
     res.status(404).render("../pages/no_user.ejs");
   } else {
--- views/converter.js
+++ views/converter.js
@@ -1,44 +1,18 @@
 import fs from "fs";
-import md2json from "md-2-json";
 import sqlite3 from "sqlite3";
-import fetch from "node-fetch";
 
 const db = new sqlite3.Database("./db/themes.db");
 
-const getFile = async () => {
-  try {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/tcstenungsund/schedule/main/md/weuweb33-22te4i.md"
-    );
-    const data = await response.text();
-    return data;
-  } catch (err) {
-    console.log("fetch error", err);
-  }
-};
-
-function getMd() {
-  (async () => {
-    const getData = await getFile();
-    console.log("data", getData);
-  })();
-}
-
-getMd();
-
-export function converter() {
-  const explorer = fs.readdirSync(
-    "C:/Users/03kaso20/Documents/web/bord/bord__backend/markdown"
-  );
+export function converter(explorer, folder, user) {
+  // const explorer = fs.readdirSync("./html/fruit");
+  // const folder = "./html/fruit/";
   const pageArr = [explorer.length];
-  const folder = "C:/Users/03kaso20/Documents/web/bord/bord__backend/markdown";
 
   for (const file in explorer) {
     const content = fs.readFileSync(folder + explorer[file], "utf8");
-    const website = md2json.parse(content);
     pageArr[file] = {
-      pageName: explorer[file].replace(".md", ""),
-      pageContent: website,
+      pageName: explorer[file].replace(".html", ""),
+      pageContent: content,
     };
   }
   console.log("Findern är körd");
@@ -51,7 +25,8 @@ export function converter() {
       pageArr[i].pageName.length + 1
     );
     let finalContent = JSON.stringify(pageArr[i].pageContent);
-    const query = `UPDATE or IGNORE molekylverkstan SET
+
+    const query = `UPDATE or IGNORE ${user} SET
       page_id = ${pageID}, page_name = '${finalName}', page_content = '${finalContent}'`;
 
     db.run(query);
