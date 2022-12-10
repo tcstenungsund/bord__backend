53c296b31209b12e4604e929e386f9a0f3f1f758
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-11 00:11:45 +0100

    Put-requests made possible (#31)

--- controller/router.js
+++ controller/router.js
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import { fetchContent } from "../model/model.js";
 import { updater } from "../views/updater.js";
+import { putCard } from "../model/model.js";
 
 const router = express.Router();
 
@@ -10,39 +11,44 @@ router.get("/", function (req, res) {
   res.render("../pages/start.ejs");
 });
 
-//* Querys the database, the table ":id"
-router.get("/:userId", async function (req, res, next) {
-  let userId = req.params.userId;
-  if (userId !== "favicon.ico") {
+//* Querys the database, the table ":userId"
+router.get("/:userId", async function (req, res) {
+  if (req.params.userId !== "favicon.ico") {
     //* Fetches data from db
-    let fetchQuery = `SELECT page_content FROM ${userId} WHERE page_name = "about";`;
-    const content = await fetchContent(fetchQuery);
+    const content = await fetchContent(
+      `SELECT page_content FROM ${req.params.userId} WHERE page_name = "about";`
+    );
     if (content == "404") {
       res.status(404).render("../pages/no_user.ejs");
     } else {
-      //* Response
       res.status(200).send(JSON.parse(content));
-
-      //* Updater
-      const explorer = fs.readdirSync(`./html/${userId}`);
-      const folder = `./html/${userId}/`;
-      updater(explorer, folder, userId);
     }
   }
-  next();
 });
 
-router.get("/:userId/:pageId", async function (req, res, next) {
-  let userId = req.params.userId;
-  let pageId = req.params.pageId;
-  let sqlQuery = `SELECT page_content FROM ${userId} WHERE page_name = "${pageId}";`;
-  const content = await fetchContent(sqlQuery);
+//* Querys the database, the table ":userId" and row ":pageID"
+router.get("/:userId/:pageId", async function (req, res) {
+  const content = await fetchContent(
+    `SELECT page_content FROM ${req.params.userId} WHERE page_name = "${req.params.pageId}";`
+  );
   if (content == "404") {
     res.status(404).render("../pages/no_page.ejs");
   } else {
     res.status(200).send(JSON.parse(content));
   }
-  next();
 });
+
+//* put-request
+router.put("/:userId", function (req, res) {
+  putCard(`UPDATE or IGNORE ${req.params.userId}
+    SET ${req.body.card_type} = "${req.body.card_id}"
+    WHERE page_name = "${req.body.page_name}"`);
+});
+
+//   //* Updater
+//   const explorer = fs.readdirSync(`./html/${userId}`);
+//   const folder = `./html/${userId}/`;
+//   updater(explorer, folder, userId);
+// });
 
 export default router;
--- model/model.js
+++ model/model.js
@@ -6,6 +6,7 @@ export function fetchContent(sqlQuery) {
   return new Promise((resolve) => {
     db.all(sqlQuery, [], (err, rows) => {
       if (err || rows.length === 0) {
+        console.log(sqlQuery);
         resolve("404");
       } else {
         rows.forEach((row) => {
@@ -14,4 +15,8 @@ export function fetchContent(sqlQuery) {
       }
     });
   });
+}
+
+export function putCard(sqlQuery) {
+  db.run(sqlQuery);
 }
