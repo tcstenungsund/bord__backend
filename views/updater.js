3b04072f8a7cf135897059d6bb2c18e0a27b048b
pontusolsson03 <58231866+pontalainen@users.noreply.github.com>
2022-12-16 12:13:53 +0100

    Removed unecessary dependencies

--- views/updater.js
+++ views/updater.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import sqlite3 from "sqlite3";
 import fetch from "node-fetch";
 import { repositories } from "./repos.js";
@@ -62,35 +61,6 @@ async function md2html() {
 
 md2html();
 
-export function updater(explorer, folder, user) {
-  const pageArr = [explorer.length];
-
-  for (const file in explorer) {
-    const content = fs.readFileSync(folder + explorer[file], "utf8");
-    pageArr[file] = {
-      pageName: explorer[file].replace(".html", ""),
-      pageContent: content,
-    };
-  }
-  console.log("Findern är körd");
-
-  let i;
-  for (i = 0; i < pageArr.length; i++) {
-    let pageID = i + 1;
-    let finalName = JSON.stringify(pageArr[i].pageName).slice(
-      1,
-      pageArr[i].pageName.length + 1
-    );
-    let finalContent = JSON.stringify(pageArr[i].pageContent);
-
-    const query = `UPDATE or IGNORE $user SET
-      page_id = $pageID, page_name = '$finalName', page_content = '$finalContent'`;
-
-    db.run(query, {
-      $pageID: pageID,
-      $finalName: finalName,
-      $finalContent: finalContent,
-    });
-  }
-  console.log("Updatern är körd");
-}
+// const query = `UPDATE or IGNORE $user SET
+//       page_id = ${pageID}, page_name = '{$pageName}', page_content = '${pageContent}'`;
+// db.run(query);
